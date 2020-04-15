f60d6c9af1035999335c86f724bddaf51d3c7acc
Yogesh Kumar <yogeshkumarbeniwal1999@gmail.com>
2020-04-16 01:55:46 +0530

    Finished Prototype1(Car)

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offSet = new Vector3(0, 3.78f, -6.04f);
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offSet;
+    }
+}
